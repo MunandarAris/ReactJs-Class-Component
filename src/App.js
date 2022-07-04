cedb398a180aeddda27d0ac31acabca0ca49e503
Aris Munandar <arismun0010@gmail.com>
2022-07-04 14:04:45 +0700

    Learn Class Component on React Js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  // initial state
+  state = {
+    counter: 0,
+  };
+
+  // === handle increment and decrement ===
+  counter = (params) => {
+    const { counter } = this.state;
+
+    if (params == "-") counter > 0 && this.setState({ counter: counter - 1 });
+    else
+      this.setState({
+        counter: counter + 1,
+      });
+  };
+  // === handle increment and decrement ===
+
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1em",
+        }}
+      >
+        <button
+          style={{
+            cursor: "pointer",
+          }}
+          onClick={() => this.counter("-")}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          -
+        </button>
+
+        <h2>{this.state.counter}</h2>
+
+        <button
+          style={{
+            cursor: "pointer",
+          }}
+          onClick={() => this.counter("+")}
+        >
+          +
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
